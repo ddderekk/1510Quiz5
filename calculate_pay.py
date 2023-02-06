ef1e5426ed51ff39597546410954544db9319cb2
Derek Tran <dtran110@my.bcit.ca>
2023-02-06 11:02:03 -0800

    Update calculate_pay function

--- calculate_pay.py
+++ calculate_pay.py
@@ -6,6 +6,19 @@ def calculate_pay(hours, wage):
     :param wage: a positive real number
     :precondition: hours must be a real number
     :precondition: wage must be a real number
-    :postcondition: calculates hourly pay for hours
-    :return: return product
+    :postcondition: calculates total weekly pay
+    :return: return weekly pay as a float
+    >>> calculate_pay(10, 10)
+    100
+    >>> calculate_pay(50, 10)
+    600
     """
+    overtime_hours = 0
+    if hours > 40:
+        overtime_hours = hours - 40
+
+    base_pay = (hours - overtime_hours) * wage
+    over_time_pay = overtime_hours * 2 * wage
+
+    total_pay = base_pay + over_time_pay
+    return total_pay
